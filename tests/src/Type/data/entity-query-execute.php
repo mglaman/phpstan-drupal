5ba2288491a10af16c4862640dadb5f3998b368a
brambaud <beram.job@gmail.com>
2022-02-25 14:37:48 +0100

    tests should fail to reproduce the issue

--- tests/src/Type/data/entity-query-execute.php
+++ tests/src/Type/data/entity-query-execute.php
@@ -50,6 +50,51 @@ $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery()
     ->accessCheck(TRUE)->count();
 assertType('int', $query->execute());
 
+$nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+assertType(
+    'array<int, string>',
+    $nodeStorage->getQuery()
+        ->accessCheck(TRUE)
+        ->execute()
+);
+$query = $nodeStorage->getQuery()
+    ->accessCheck(TRUE);
+assertType('array<int, string>', $query->execute());
+assertType(
+    'int',
+    $nodeStorage->getQuery()
+        ->accessCheck(TRUE)
+        ->count()
+        ->execute()
+);
+$query = $nodeStorage->getQuery()
+    ->accessCheck(TRUE)
+    ->count();
+assertType('int', $query->execute());
+
+/** @var \Drupal\node\NodeStorage $typedNodeStorage */
+$typedNodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+assertType(
+    'array<int, string>',
+    $typedNodeStorage->getQuery()
+        ->accessCheck(TRUE)
+        ->execute()
+);
+$query = $typedNodeStorage->getQuery()
+    ->accessCheck(TRUE);
+assertType('array<int, string>', $query->execute());
+assertType(
+    'int',
+    $typedNodeStorage->getQuery()
+        ->accessCheck(TRUE)
+        ->count()
+        ->execute()
+);
+$query = $typedNodeStorage->getQuery()
+    ->accessCheck(TRUE)
+    ->count();
+assertType('int', $query->execute());
+
 assertType(
     'array<string, string>',
     \Drupal::entityTypeManager()->getStorage('block')->getQuery()
