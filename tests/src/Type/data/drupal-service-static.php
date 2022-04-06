61902bdf1ecf5cead251f1a1000ee409217f90e9
Matt Glaman <nmd.matt@gmail.com>
2022-04-06 15:47:57 -0500

    Fix test data after adding classmap

--- tests/src/Type/data/drupal-service-static.php
+++ tests/src/Type/data/drupal-service-static.php
@@ -5,4 +5,6 @@ namespace DrupalServiceStatic;
 use Drupal\service_map\MyService;
 use function PHPStan\Testing\assertType;
 
-assertType(MyService::class, \Drupal::service('service_map.my_service'));
+function test(): void {
+    assertType(MyService::class, \Drupal::service('service_map.my_service'));
+}
