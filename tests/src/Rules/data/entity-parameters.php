c44b3e52c64050a33a4385c9b97ad93f877cfe79
Matt Glaman <nmd.matt@gmail.com>
2022-05-24 16:45:38 -0500

    Add stubs for ContentEntityInterface and FieldableEntityInterface
    Fixes #412 and provides iterable template for content entities.

---
+++ tests/src/Rules/data/entity-parameters.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ContentEntityParameters;
+
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\user\Entity\User;
+
+function node_insert(\Drupal\node\NodeInterface $node): void {
+
+}
+function user_insert(User $user): void {
+
+}
+
+function config_insert(EntityViewDisplay $viewDisplay): void {
+
+}
