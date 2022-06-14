55628e3c87aec138f1434e0d09ffb7545c19ec8f
Laytan Laats <laytan.laats@republic-m.com>
2022-06-14 19:32:44 +0200

    add support for class string service ids

--- tests/src/Type/data/drupal-service-static.php
+++ tests/src/Type/data/drupal-service-static.php
@@ -7,4 +7,5 @@ use function PHPStan\Testing\assertType;
 
 function test(): void {
     assertType(MyService::class, \Drupal::service('service_map.my_service'));
+    assertType(MyService::class, \Drupal::service(MyService::class));
 }
