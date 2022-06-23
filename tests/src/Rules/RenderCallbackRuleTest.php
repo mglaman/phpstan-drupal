c4ac3c5168b3841b8df3c7817b3aa6197e1711b9
Dave Long <dave@longwaveconsulting.com>
2022-06-23 18:51:57 +0100

    Clear errors from fixed test cases.

--- tests/src/Rules/RenderCallbackRuleTest.php
+++ tests/src/Rules/RenderCallbackRuleTest.php
@@ -65,13 +65,7 @@ final class RenderCallbackRuleTest extends DrupalRuleTestCase {
         ];
         yield [
             __DIR__ . '/../../fixtures/drupal/modules/pre_render_callback_rule/src/RenderArrayWithPreRenderCallback.php',
-            [
-                [
-                    "#pre_render value 'non-empty-string' at key '3' is invalid.",
-                    19,
-                    "Refactor concatenation of `static::class` with method name to an array callback: [static::class, 'preRenderCallback']"
-                ]
-            ]
+            []
         ];
         yield [
             __DIR__ . '/../../fixtures/drupal/modules/pre_render_callback_rule/src/RenderCallbackInterfaceObject.php',
@@ -79,13 +73,7 @@ final class RenderCallbackRuleTest extends DrupalRuleTestCase {
         ];
         yield [
             __DIR__ . '/../../fixtures/drupal/modules/pre_render_callback_rule/src/LazyBuilderWithConstant.php',
-            [
-                [
-                    "#lazy_builder value 'non-empty-string' at key '0' is invalid.",
-                    25,
-                    "Refactor concatenation of `static::class` with method name to an array callback: [static::class, 'lazyBuilder']"
-                ]
-            ]
+            []
         ];
         yield [
             __DIR__ . '/../../fixtures/drupal/modules/pre_render_callback_rule/src/FormWithClosure.php',
