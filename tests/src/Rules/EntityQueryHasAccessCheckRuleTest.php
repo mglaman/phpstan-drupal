dbaaea71ec19ad7c7c304d251532e3f1354e10a8
Frank Ebbers <sheetmusicfrank@gmail.com>
2023-01-05 14:32:37 +0100

    Let's test all permutations

--- tests/src/Rules/EntityQueryHasAccessCheckRuleTest.php
+++ tests/src/Rules/EntityQueryHasAccessCheckRuleTest.php
@@ -99,8 +99,23 @@ final class EntityQueryHasAccessCheckRuleTest extends DrupalRuleTestCase
 
         yield 'bug-494.php' => [
             [__DIR__.'/data/bug-494.php'],
-            []
+            [
+                [
+                    'Relying on entity queries to check access by default is deprecated in drupal:9.2.0 and an error will be thrown from drupal:10.0.0. Call \Drupal\Core\Entity\Query\QueryInterface::accessCheck() with TRUE or FALSE to specify whether access should be checked.',
+                    43,
+                    'See https://www.drupal.org/node/3201242',
+                ],
+                [
+                    'Relying on entity queries to check access by default is deprecated in drupal:9.2.0 and an error will be thrown from drupal:10.0.0. Call \Drupal\Core\Entity\Query\QueryInterface::accessCheck() with TRUE or FALSE to specify whether access should be checked.',
+                    103,
+                    'See https://www.drupal.org/node/3201242',
+                ],
+                [
+                    'Relying on entity queries to check access by default is deprecated in drupal:9.2.0 and an error will be thrown from drupal:10.0.0. Call \Drupal\Core\Entity\Query\QueryInterface::accessCheck() with TRUE or FALSE to specify whether access should be checked.',
+                    108,
+                    'See https://www.drupal.org/node/3201242',
+                ],
+            ]
         ];
-
     }
 }
