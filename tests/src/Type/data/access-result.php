d5010b7c55483577e0b6f497d9d8fbc5e48885b7
Matt Glaman <nmd.matt@gmail.com>
2023-06-14 14:56:14 -0500

    AccessResult stubs (#573)

---
+++ tests/src/Type/data/access-result.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AccessResultType;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultAllowed;
+use Drupal\Core\Access\AccessResultForbidden;
+use Drupal\Core\Access\AccessResultNeutral;
+use function PHPStan\Testing\assertType;
+
+assertType(AccessResultAllowed::class, AccessResult::allowedIf(true));
+assertType(AccessResultNeutral::class, AccessResult::allowedIf(false));
+
+assertType(AccessResultForbidden::class, AccessResult::forbiddenIf(true));
+assertType(AccessResultNeutral::class, AccessResult::forbiddenIf(false));
