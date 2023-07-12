04ad273aa759b97a9511b0f09f9eb5c24c7c59c9
Simon Bäse <simon@youvo.org>
2023-07-12 20:15:11 +0200

    Add TranslatableInterface stub (#588)
    Backports return type fixes from https://www.drupal.org/project/drupal/issues/3269177

---
+++ tests/src/Type/ContentEntityTranslationTypeTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace mglaman\PHPStanDrupal\Tests\Type;
+
+use mglaman\PHPStanDrupal\Tests\AdditionalConfigFilesTrait;
+use PHPStan\Testing\TypeInferenceTestCase;
+
+final class ContentEntityTranslationTypeTest extends TypeInferenceTestCase
+{
+    use AdditionalConfigFilesTrait;
+
+    public function dataFileAsserts(): iterable
+    {
+        yield from self::gatherAssertTypes(__DIR__ . '/data/content-entity-translation.php');
+    }
+
+    /**
+     * @dataProvider dataFileAsserts
+     * @param string $assertType
+     * @param string $file
+     * @param mixed ...$args
+     */
+    public function testFileAsserts(
+        string $assertType,
+        string $file,
+        ...$args
+    ): void {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+}
