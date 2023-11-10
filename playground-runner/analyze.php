3de02cc3651139e866656d1eb485d5f3d08fb7b8
Matt Glaman <nmd.matt@gmail.com>
2023-11-10 08:46:47 -0600

    Add playground-runner

---
+++ playground-runner/analyze.php
@@ -0,0 +1,189 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPStan\AnalysedCodeException;
+use PHPStan\Analyser\Error;
+use PHPStan\Analyser\RuleErrorTransformer;
+use PHPStan\Analyser\ScopeContext;
+use PHPStan\Analyser\ScopeFactory;
+use PHPStan\BetterReflection\NodeCompiler\Exception\UnableToCompileNode;
+use PHPStan\BetterReflection\Reflection\Exception\CircularReference;
+use PHPStan\BetterReflection\Reflector\Exception\IdentifierNotFound;
+use PHPStan\Collectors\CollectedData;
+use PHPStan\Node\CollectedDataNode;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+
+require __DIR__.'/vendor/autoload.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$phpstanVersion = \Composer\InstalledVersions::getPrettyVersion('phpstan/phpstan');
+$phpstanDrupalVersion = \Composer\InstalledVersions::getPrettyVersion('mglaman/phpstan-drupal');
+$drupalCoreVersion = \Composer\InstalledVersions::getPrettyVersion('drupal/core');
+
+/**
+ * @param CollectedData[] $collectedData
+ * @return Error[]
+ */
+function getCollectedDataErrors(\PHPStan\DependencyInjection\Container $container, array $collectedData): array
+{
+	$nodeType = CollectedDataNode::class;
+	$node = new CollectedDataNode($collectedData, true);
+	$file = 'N/A';
+	$scope = $container->getByType(ScopeFactory::class)->create(ScopeContext::create($file));
+	$ruleRegistry = $container->getByType(\PHPStan\Rules\Registry::class);
+	$ruleErrorTransformer = $container->getByType(RuleErrorTransformer::class);
+	$errors = [];
+	foreach ($ruleRegistry->getRules($nodeType) as $rule) {
+		try {
+			$ruleErrors = $rule->processNode($node, $scope);
+		} catch (AnalysedCodeException $e) {
+			$errors[] = new Error($e->getMessage(), $file, $node->getLine(), $e, null, null, $e->getTip());
+			continue;
+		} catch (IdentifierNotFound $e) {
+			$errors[] = new Error(sprintf('Reflection error: %s not found.', $e->getIdentifier()->getName()), $file, $node->getLine(), $e, null, null, 'Learn more at https://phpstan.org/user-guide/discovering-symbols');
+			continue;
+		} catch (UnableToCompileNode | CircularReference $e) {
+			$errors[] = new Error(sprintf('Reflection error: %s', $e->getMessage()), $file, $node->getLine(), $e);
+			continue;
+		}
+
+		foreach ($ruleErrors as $ruleError) {
+			$errors[] = $ruleErrorTransformer->transform($ruleError, $scope, $nodeType, $node->getLine());
+		}
+	}
+
+	return $errors;
+}
+
+function clearTemp($tmpDir): void
+{
+	$files = new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator($tmpDir, RecursiveDirectoryIterator::SKIP_DOTS),
+		RecursiveIteratorIterator::CHILD_FIRST
+	);
+
+	foreach ($files as $fileinfo) {
+		$todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+		$todo($fileinfo->getRealPath());
+	}
+}
+
+return function(array $event) use ($phpstanVersion, $phpstanDrupalVersion, $drupalCoreVersion) {
+    $tmpDir = sys_get_temp_dir() . '/phpstan-runner';
+    if (!is_dir($tmpDir)) {
+        mkdir($tmpDir);
+    }
+	clearTemp($tmpDir);
+	$code = $event['code'];
+	$level = $event['level'];
+	$codePath = sys_get_temp_dir() . '/phpstan-runner/tmp.php';
+	file_put_contents($codePath, $code);
+
+	$rootDir = __DIR__;
+    $configFiles = [
+		$rootDir . '/playground.neon',
+        $rootDir . '/vendor/mglaman/phpstan-drupal/extension.neon',
+        $rootDir . '/vendor/mglaman/phpstan-drupal/rules.neon',
+        $rootDir . '/vendor/phpstan/phpstan-deprecation-rules/rules.neon',
+	];
+	foreach ([
+		'strictRules' => $rootDir . '/vendor/phpstan/phpstan-strict-rules/rules.neon',
+		'bleedingEdge' => 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/conf/bleedingEdge.neon',
+	] as $key => $file) {
+		if (!isset($event[$key]) || !$event[$key]) {
+			continue;
+		}
+
+		$configFiles[] = $file;
+	}
+	$finalConfigFile = $tmpDir . '/run-phpstan-tmp.neon';
+	$neon = \Nette\Neon\Neon::encode([
+		'includes' => $configFiles,
+		'parameters' => [
+			'inferPrivatePropertyTypeFromConstructor' => true,
+			'treatPhpDocTypesAsCertain' => $event['treatPhpDocTypesAsCertain'] ?? true,
+			'phpVersion' => $event['phpVersion'] ?? 80000,
+			'featureToggles' => [
+				'disableRuntimeReflectionProvider' => true,
+			],
+            'drupal' => [
+                'drupal_root' => \Composer\InstalledVersions::getInstallPath('drupal/core'),
+            ],
+		],
+		'services' => [
+			'currentPhpVersionSimpleParser!' => [
+				'factory' => '@currentPhpVersionRichParser',
+			],
+		],
+	]);
+	file_put_contents($finalConfigFile, $neon);
+
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/ReflectionUnionType.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/ReflectionIntersectionType.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/ReflectionAttribute.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/Attribute.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/Enum/UnitEnum.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/Enum/BackedEnum.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/Enum/ReflectionEnum.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/Enum/ReflectionEnumUnitCase.php';
+	require_once 'phar://' . $rootDir . '/vendor/phpstan/phpstan/phpstan.phar/stubs/runtime/Enum/ReflectionEnumBackedCase.php';
+
+	$containerFactory = new \PHPStan\DependencyInjection\ContainerFactory($tmpDir);
+	$container = $containerFactory->create($tmpDir, [sprintf('%s/config.level%s.neon', $containerFactory->getConfigDirectory(), $level), $finalConfigFile], [$codePath]);
+
+    // Note: this is the big change from the parent script in phpstan-src.
+    foreach ($container->getParameter('bootstrapFiles') as $bootstrapFileFromArray) {
+        try {
+            (static function (string $bootstrapFileFromArray) use ($container): void {
+                require_once $bootstrapFileFromArray;
+            })($bootstrapFileFromArray);
+        } catch (Throwable $e) {
+            $error = sprintf('%s thrown in %s on line %d while loading bootstrap file %s: %s', get_class($e), $e->getFile(), $e->getLine(), $file, $e->getMessage());
+            return ['result' => [$error], 'versions' => [
+                'phpstan' => $phpstanVersion,
+                'phpstan-drupal' => $phpstanDrupalVersion,
+                'drupal' => $drupalCoreVersion,
+            ]];
+        }
+    }
+
+	/** @var \PHPStan\Analyser\Analyser $analyser */
+	$analyser = $container->getByType(\PHPStan\Analyser\Analyser::class);
+	$analyserResult = $analyser->analyse([$codePath], null, null, false, [$codePath]);
+	$hasInternalErrors = count($analyserResult->getInternalErrors()) > 0 || $analyserResult->hasReachedInternalErrorsCountLimit();
+	$results = $analyserResult->getErrors();
+
+    if (!$hasInternalErrors) {
+		foreach (getCollectedDataErrors($container, $analyserResult->getCollectedData()) as $error) {
+			$results[] = $error;
+		}
+	}
+
+	error_clear_last();
+
+	$errors = [];
+	$tipFormatter = new OutputFormatter(false);
+	foreach ($results as $result) {
+		$error = [
+			'message' => $result->getMessage(),
+			'line' => $result->getLine(),
+			'ignorable' => $result->canBeIgnored(),
+		];
+		if ($result->getTip() !== null) {
+			$error['tip'] = $tipFormatter->format($result->getTip());
+		}
+		if ($result->getIdentifier() !== null) {
+			$error['identifier'] = $result->getIdentifier();
+		}
+		$errors[] = $error;
+	}
+
+	return ['result' => $errors, 'versions' => [
+        'phpstan' => $phpstanVersion,
+        'phpstan-drupal' => $phpstanDrupalVersion,
+        'drupal' => $drupalCoreVersion,
+    ]];
+};
