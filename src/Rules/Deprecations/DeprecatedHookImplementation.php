e6ae68f8d8a2b66e3c45a7b18ece2af14d19457b
Matt Glaman <nmd.matt@gmail.com>
2023-11-14 14:14:09 -0600

    Rename bleeding edge toggle, hardcoded core deprecated hooks

--- src/Rules/Deprecations/DeprecatedHookImplementation.php
+++ src/Rules/Deprecations/DeprecatedHookImplementation.php
@@ -13,10 +13,7 @@ use PHPStan\Rules\RuleErrorBuilder;
 class DeprecatedHookImplementation implements Rule
 {
 
-    /**
-     * @var \PHPStan\Reflection\ReflectionProvider
-     */
-    protected $reflectionProvider;
+    protected ReflectionProvider $reflectionProvider;
 
     public function __construct(ReflectionProvider $reflectionProvider)
     {
@@ -45,6 +42,36 @@ class DeprecatedHookImplementation implements Rule
 
         $function_name = $node->name->toString();
         $hook_name = substr_replace($function_name, "hook", 0, strlen($module_name));
+
+        // @todo replace this hardcoded logic with something more intelligent and extensible.
+        if ($hook_name === 'hook_field_widget_form_alter') {
+            return [
+                RuleErrorBuilder::message(
+                    "Function $function_name implements $hook_name which is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use hook_field_widget_single_element_form_alter instead.",
+                )->build()
+            ];
+        }
+        if (str_starts_with($hook_name, 'hook_field_widget_') && str_ends_with($hook_name, '_form_alter')) {
+            return [
+                RuleErrorBuilder::message(
+                    "Function $function_name implements $hook_name which is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use hook_field_widget_single_element_WIDGET_TYPE_form_alter instead.",
+                )->build()
+            ];
+        }
+        if ($hook_name === 'hook_field_widget_multivalue_form_alter') {
+            return [
+                RuleErrorBuilder::message(
+                    "Function $function_name implements $hook_name which is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use hook_field_widget_complete_form_alter instead.",
+                )->build()
+            ];
+        }
+        if (str_starts_with($hook_name, 'hook_field_widget_multivalue_') && str_ends_with($hook_name, '_form_alter')) {
+            return [
+                RuleErrorBuilder::message(
+                    "Function $function_name implements $hook_name which is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use hook_field_widget_complete_WIDGET_TYPE_form_alter instead.",
+                )->build()
+            ];
+        }
 
         $hook_name_node = new Name($hook_name);
         if (!$this->reflectionProvider->hasFunction($hook_name_node, $scope)) {
