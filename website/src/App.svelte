1e7b56bc2d2b93b8a33f663252b44d6ce99ce44b
Matt Glaman <nmd.matt@gmail.com>
2023-11-21 14:42:51 -0600

    add github link on playground

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -162,7 +162,14 @@
 <div class="p-2 md:p-4 lg:py-8 lg:px-0">
     <main class="max-w-5xl md:px-6 mx-auto bg-white rounded-lg">
         <div class="p-4 md:p-6 lg:p-8">
-            <h1 class="font-bold md:text-4xl sm:text-5xl text-4xl text-gray-900 tracking-tight">PHPStan Drupal Extension Playground</h1>
+            <div class="flex flex-row items-center">
+                <h1 class="font-bold md:text-4xl sm:text-5xl text-4xl text-gray-900 tracking-tight">PHPStan Drupal Extension Playground</h1>
+                <a target="_blank" href="https://github.com/mglaman/phpstan-drupal" class="border-b-2 border-transparent duration-150 ease-in-out focus:outline-none focus:text-gray-700 font-medium hover:text-gray-700 leading-5 ml-4 pt-1 px-1 text-gray-500 text-md transition" rel="noopener noreferrer">
+                    <svg class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            </div>
             <p class="my-4">Try out PHPStan with phpstan-drupal and all of its features here in the editor. <a href="https://phpstan.org/" class="hover:no-underline underline">Learn more about PHPStan »</a></p>
             <form class="space-y-4" on:submit={analyse}>
                 <Editor code={data.code} errors={result?.tabs[0]?.errors} />
