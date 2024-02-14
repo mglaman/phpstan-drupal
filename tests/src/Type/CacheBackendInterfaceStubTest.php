a33689a82b4929c8d9cb2b26517a694485879cf2
Matt Glaman <nmd.matt@gmail.com>
2024-02-13 21:38:36 -0600

    Define stub for cache object shape (#724)
    * Define stub for cache object shape
    
    Fixes #270
    
    * Keep PHP 7.4 support

---
+++ tests/src/Type/CacheBackendInterfaceStubTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace mglaman\PHPStanDrupal\Tests\Type;
+
+use mglaman\PHPStanDrupal\Tests\AdditionalConfigFilesTrait;
+use PHPStan\Testing\TypeInferenceTestCase;
+
+final class CacheBackendInterfaceStubTest extends TypeInferenceTestCase
+{
+    use AdditionalConfigFilesTrait;
+
+    public function dataFileAsserts(): iterable
+    {
+        yield from self::gatherAssertTypes(__DIR__ . '/data/cache-backend-interface-stub.php');
+    }
+
+    /**
+     * @dataProvider dataFileAsserts
+     * @param string $assertType
+     * @param string $file
+     * @param mixed ...$args
+     */
+    public function testFileAsserts(
+        string $assertType,
+        string $file,
+        ...$args
+    ): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+}
