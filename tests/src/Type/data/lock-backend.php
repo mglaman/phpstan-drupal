3b30fac129e63ccdefb4422d8360e5e28ecf451e
Matt Glaman <nmd.matt@gmail.com>
2024-03-20 05:57:31 -0500

    fix assertions

--- tests/src/Type/data/lock-backend.php
+++ tests/src/Type/data/lock-backend.php
@@ -7,8 +7,8 @@ use function PHPStan\Testing\assertType;
 $lock = \Drupal::lock();
 
 if (!$lock->acquire('foo')) {
-    assertType('false', $lock->acquire('foo'));
+    assertType('bool', $lock->acquire('foo'));
     if ($lock->acquire('foo')) {
-        assertType('true', $lock->acquire('foo'));
+        assertType('bool', $lock->acquire('foo'));
     }
 }
