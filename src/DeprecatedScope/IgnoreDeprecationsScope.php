1dd054edc89333e765a0d1531389cde5a34773fd
mondrake <mondrake.org@gmail.com>
2024-07-07 16:11:22 +0200

    fix

--- src/DeprecatedScope/IgnoreDeprecationsScope.php
+++ src/DeprecatedScope/IgnoreDeprecationsScope.php
@@ -7,7 +7,6 @@ namespace mglaman\PHPStanDrupal\DeprecatedScope;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Deprecations\DeprecatedScopeResolver;
 use PHPUnit\Framework\Attributes\IgnoreDeprecations;
-use function strpos;
 
 final class IgnoreDeprecationsScope implements DeprecatedScopeResolver
 {
@@ -15,7 +14,7 @@ final class IgnoreDeprecationsScope implements DeprecatedScopeResolver
     public function isScopeDeprecated(Scope $scope): bool
     {
         if ($scope->isInClass()) {
-            $class = $scope->getClassReflection();
+            $class = $scope->getClassReflection()->getNativeReflection();
             if ($class->getAttributes(IgnoreDeprecations::class) !== []) {
                 return true;
             }
