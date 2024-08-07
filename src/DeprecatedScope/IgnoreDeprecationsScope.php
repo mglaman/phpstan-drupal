3a7a3a23d208b1e22e6fa4a3b93980871c7419c6
Matt Glaman <nmd.matt@gmail.com>
2024-08-07 16:04:15 -0500

    use class_exists for checking attribute class

--- src/DeprecatedScope/IgnoreDeprecationsScope.php
+++ src/DeprecatedScope/IgnoreDeprecationsScope.php
@@ -13,6 +13,10 @@ final class IgnoreDeprecationsScope implements DeprecatedScopeResolver
 
     public function isScopeDeprecated(Scope $scope): bool
     {
+        if (!class_exists(IgnoreDeprecations::class)) {
+            return false;
+        }
+
         if ($scope->isInClass()) {
             $class = $scope->getClassReflection()->getNativeReflection();
             if ($class->getAttributes(IgnoreDeprecations::class) !== []) {
