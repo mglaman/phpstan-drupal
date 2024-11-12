80b6a4abdd0fb66c005cea0252cee859a93f8bd2
Frank Ebbers <sheetmusicfrank@gmail.com>
2024-11-12 09:45:39 +0100

    PHPUnit D9 fix

--- src/Rules/Deprecations/DeprecatedHookImplementation.php
+++ src/Rules/Deprecations/DeprecatedHookImplementation.php
@@ -63,8 +63,8 @@ class DeprecatedHookImplementation implements Rule
             if (str_starts_with($hook_name, 'hook_field_widget_') && str_ends_with($hook_name, '_form_alter')) {
                 return $this->buildError(
                     $function_name,
-                    'useHookFieldWidgetWidgetTypeFormAlter',
                     'hook_field_widget_WIDGET_TYPE_form_alter',
+                    'useHookFieldWidgetWidgetTypeFormAlter',
                     'in drupal:9.2.0 and is removed from drupal:10.0.0. Use hook_field_widget_single_element_WIDGET_TYPE_form_alter instead.'
                 );
             }
