7df544fefb5830bc0b8175b24553717095ee8941
Niklan <niklanrus@gmail.com>
2025-02-13 22:14:33 +0500

    WIP

--- src/Type/InspectorTypeExtension.php
+++ src/Type/InspectorTypeExtension.php
@@ -13,11 +13,17 @@ use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierAwareExtension;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Accessory\HasOffsetType;
 use PHPStan\Type\ArrayType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\IntegerRangeType;
 use PHPStan\Type\IterableType;
 use PHPStan\Type\MixedType;
+use PHPStan\Type\ObjectType;
 use PHPStan\Type\StaticMethodTypeSpecifyingExtension;
 use PHPStan\Type\StringType;
+use PHPStan\Type\TypeCombinator;
+use Stringable;
 use function in_array;
 
 final class InspectorTypeExtension implements StaticMethodTypeSpecifyingExtension, TypeSpecifierAwareExtension
@@ -39,7 +45,11 @@ final class InspectorTypeExtension implements StaticMethodTypeSpecifyingExtensio
     {
         $implemented_methods = [
           'assertAllStrings',
+          'assertAllStringable',
           'assertAllArrays',
+          'assertStrictArray',
+          'assertAllStrictArrays',
+          'assertAllHaveKey',
         ];
 
         return in_array($staticMethodReflection->getName(), $implemented_methods);
@@ -50,31 +60,93 @@ final class InspectorTypeExtension implements StaticMethodTypeSpecifyingExtensio
         return match ($staticMethodReflection->getName()) {
             default => throw new InvalidArgumentException(sprintf('Method %s is not yet implemented.', $staticMethodReflection->getName())),
             'assertAllStrings' => $this->specifyAssertAllStrings($staticMethodReflection, $node, $scope, $context),
+            'assertAllStringable' => $this->specifyAssertAllStringable($staticMethodReflection, $node, $scope, $context),
             'assertAllArrays' => $this->specifyAssertAllArrays($staticMethodReflection, $node, $scope, $context),
+            'assertStrictArray' => $this->specifyAssertStrictArray($staticMethodReflection, $node, $scope, $context),
+            'assertAllStrictArrays' => $this->specifyAssertAllStrictArrays($staticMethodReflection, $node, $scope, $context),
+            'assertAllHaveKey' => $this->specifyAssertAllHaveKey($staticMethodReflection, $node, $scope, $context),
         };
     }
 
     private function specifyAssertAllStrings(MethodReflection $staticMethodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
     {
-        if (!isset($node->getArgs()[0])) {
-            return new SpecifiedTypes();
-        }
-
         $newType = new IterableType(new MixedType(true), new StringType());
 
         return $this->typeSpecifier->create($node->getArgs()[0]->value, $newType, $context, $scope);
     }
 
+    private function specifyAssertAllStringable(MethodReflection $staticMethodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
+    {
+        $arg = $node->getArgs()[0]->value;
+
+        // Drupal considers string as part of "stringable" as well.
+        $stringable = TypeCombinator::union(new ObjectType(Stringable::class), new StringType());
+        $newType = new IterableType(new MixedType(true), $stringable);
+
+        return $this->typeSpecifier->create($arg, $newType, $context, $scope);
+    }
+
     private function specifyAssertAllArrays(MethodReflection $staticMethodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
     {
-        if (!isset($node->getArgs()[0])) {
-            return new SpecifiedTypes();
-        }
-    
         $arg = $node->getArgs()[0]->value;
         $arrayType = new ArrayType(new MixedType(true), new MixedType(true));
         $newType = new IterableType(new MixedType(true), $arrayType);
 
         return $this->typeSpecifier->create($arg, $newType, $context, $scope);
+    }
+
+    private function specifyAssertStrictArray(MethodReflection $staticMethodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
+    {
+        $arg = $node->getArgs()[0]->value;
+        $newType = new ArrayType(IntegerRangeType::createAllGreaterThanOrEqualTo(0), new MixedType(true));
+
+        return $this->typeSpecifier->create($arg, $newType, $context, $scope);
+    }
+
+    private function specifyAssertAllStrictArrays(MethodReflection $staticMethodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
+    {
+        $arg = $node->getArgs()[0]->value;
+        $newType = new IterableType(new MixedType(true), new ArrayType(IntegerRangeType::createAllGreaterThanOrEqualTo(0), new MixedType(true)));
+
+        return $this->typeSpecifier->create($arg, $newType, $context, $scope);
+    }
+
+    private function specifyAssertAllHaveKey(MethodReflection $staticMethodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
+    {
+        $args = $node->getArgs();
+
+        $traversableArg = $args[0]->value;
+        $traversableType = $scope->getType($traversableArg);
+
+        if (!$traversableType instanceof ArrayType) {
+            return new SpecifiedTypes();
+        }
+
+        $keys = [];
+        foreach ($args as $delta => $arg) {
+            if ($delta === 0) {
+                continue;
+            }
+
+            $argType = $scope->getType($arg->value);
+            if (!$argType instanceof ConstantStringType) {
+                return new SpecifiedTypes();
+            }
+
+            $keys[] = $argType->getValue();
+        }
+
+        if (empty($keys)) {
+            return new SpecifiedTypes();
+        }
+
+        $itemType = $traversableType->getItemType();
+        foreach ($keys as $key) {
+            $itemType = TypeCombinator::intersect($itemType, new HasOffsetType(new ConstantStringType($key)));
+        }
+
+        $newArrayType = new ArrayType($traversableType->getKeyType(), $itemType);
+
+        return $this->typeSpecifier->create($traversableArg, $newArrayType, $context, $scope);
     }
 }
--- tests/src/Type/data/inspector.php
+++ tests/src/Type/data/inspector.php
@@ -18,9 +18,29 @@ assert(Inspector::assertAllStrings($input));
 assertType('iterable<string>', $input);
 assertType('string', iterator_to_array($input, FALSE)[0]);
 
+// Inspector::assertAllStringable()
+class StringableClass implements \Stringable {
+    public function __toString(): string {
+      return 'foo';
+    }
+}
+
+function mixed_string_and_stringable(): mixed {
+  return ['foo', new StringableClass()];
+}
+
+$input = [];
+assert(Inspector::assertAllStringable($input));
+assertType('array{}', $input);
+
+$input = mixed_string_and_stringable();
+assert(Inspector::assertAllStringable($input));
+assertType('iterable<string|Stringable>', $input);
+assertType('string|Stringable', iterator_to_array($input, FALSE)[0]);
+
 // Inspector::assertAllArrays()
 function mixed_array(): mixed {
-  return [[],  []];
+  return [[], []];
 }
 
 $input = [];
@@ -31,3 +51,44 @@ $input = mixed_array();
 $result = Inspector::assertAllArrays($input);
 \assert(Inspector::assertAllArrays($input));
 assertType('iterable<array<mixed, mixed>>', $input);
+
+// Inspector::assertStrictArray()
+function mixed_strict_array(): mixed {
+  return ['foo', 'bar'];
+}
+
+$input = [];
+assert(Inspector::assertStrictArray($input));
+assertType('array{}', $input);
+
+$input = mixed_strict_array();
+assert(Inspector::assertStrictArray($input));
+assertType('array<int<0, max>, mixed>', $input);
+assertType('mixed', $input[0]);
+
+// Inspector::assertAllStrictArrays()
+function mixed_all_strict_arrays(): mixed {
+  return [['foo', 'bar'], ['foo', 'bar']];
+}
+
+$input = [];
+assert(Inspector::assertStrictArray($input));
+assertType('array{}', $input);
+
+$input = mixed_all_strict_arrays();
+assert(Inspector::assertAllStrictArrays($input));
+assertType('iterable<array<int<0, max>, mixed>>', $input);
+assertType('array<int<0, max>, mixed>', iterator_to_array($input, FALSE)[0]);
+
+// Inspector::assertAllHaveKey()
+function mixed_keyed_arrays(): mixed {
+  return [['foo' => 'bar'], ['baz' => 'bar']];
+}
+
+$input = [];
+assert(Inspector::assertAllHaveKey($input));
+assertType('array{}', $input);
+
+$input = mixed_keyed_arrays();
+assert(Inspector::assertAllHaveKey($input, 'foo', 'baz'));
+assertType('array{}', $input);
