96bdef1394283c22dd268635cc816f235ad94466
Niklan <niklanrus@gmail.com>
2025-04-15 20:32:47 +0500

    Update test cases with updated 'Inspector'

--- tests/src/Type/data/bug-857.php
+++ tests/src/Type/data/bug-857.php
@@ -5,16 +5,17 @@ use function PHPStan\Testing\assertType;
 
 function mixed_function(): mixed {}
 
-// If the type is not narrowed from mixed, it is narrowed to 'iterable'.
+// If the type is not narrowed from 'mixed' before call, it is narrowed to
+// 'iterable'.
 $array = mixed_function();
 assertType('mixed', $array);
-Inspector::assertAll(fn (array $i): bool => TRUE, $array);
+\assert(Inspector::assertAll(fn (array $i): bool => TRUE, $array));
 assertType('iterable', $array);
 
 // If the type is narrowed before '::assertAll()', it shouldn't change it.
 $array = mixed_function();
 assertType('mixed', $array);
-Inspector::assertAllArrays($array);
+\assert(Inspector::assertAllArrays($array));
 assertType('iterable<array>', $array);
-Inspector::assertAll(fn (array $i): bool => TRUE, $array);
+\assert(Inspector::assertAll(fn (array $i): bool => TRUE, $array));
 assertType('iterable<array>', $array);
