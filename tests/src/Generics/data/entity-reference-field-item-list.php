c3ff6f6e4ca5cfdae67012662eb0655f0346fe9d
Niklan <niklanrus@gmail.com>
2025-04-24 20:45:11 +0500

    EntityReferenceFormatterBase stub

--- tests/src/Generics/data/entity-reference-field-item-list.php
+++ tests/src/Generics/data/entity-reference-field-item-list.php
@@ -13,8 +13,6 @@ function defaultItemList(EntityReferenceFieldItemList $items): void {
     assertType('Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem<Drupal\Core\Entity\EntityInterface>|null', $items->offsetGet(0));
     foreach ($items as $item) {
         assertType('Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem<Drupal\Core\Entity\EntityInterface>', $item);
-        assertType('Drupal\Core\Entity\EntityInterface|null', $item->entity);
-        assertType('int|string|null', $item->target_id);
     }
     assertType('array<int, Drupal\Core\Entity\EntityInterface>', $items->referencedEntities());
     foreach ($items->referencedEntities() as $entity) {
@@ -32,8 +30,6 @@ function phpDocOverride(EntityReferenceFieldItemList $items): void {
     assertType('Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem<Drupal\node\NodeInterface>|null', $items->offsetGet(0));
     foreach ($items as $item) {
         assertType('Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem<Drupal\node\NodeInterface>', $item);
-        assertType('Drupal\node\NodeInterface|null', $item->entity);
-        assertType('int|string|null', $item->target_id);
     }
     assertType('array<int, Drupal\node\NodeInterface>', $items->referencedEntities());
     foreach ($items->referencedEntities() as $entity) {
