091633d71f637d696aa39cf37a808017e6992af8
dpi <pro@danielph.in>
2025-08-05 16:02:39 +0800

    ✨ Narrow return value of getKey by whether hasKey was called beforehand

--- tests/src/Type/data/entity-type-stubs.php
+++ tests/src/Type/data/entity-type-stubs.php
@@ -3,6 +3,7 @@
 namespace EntityTypeStubs;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityType;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\node\Entity\Node;
 
@@ -15,3 +16,24 @@ function (ContentEntityInterface $entity): void
         assertType(FieldItemListInterface::class, $field);
     }
 };
+
+assert($entityTypeDefault instanceof EntityType);
+assertType('string|false', $entityTypeDefault->getKey('foo'));
+
+assert($noKey instanceof EntityType);
+assert($noKey->hasKey('foo') === FALSE);
+assertType('false', $noKey->getKey('foo'));
+
+assert($hasKey instanceof EntityType);
+assert($hasKey->hasKey('foo') === TRUE);
+assertType('string', $hasKey->getKey('foo'));
+
+// Test getting a key doesn't affect another key.
+assert($entityType instanceof EntityType);
+assert($entityType->hasKey('foo') === TRUE);
+assertType('string', $entityType->getKey('foo'));
+// A different arg that wasn't narrowed previously:
+assertType('string|false', $entityType->getKey('bar'));
+// ...until we know better:
+assert($entityType->hasKey('bar') === TRUE);
+assertType('string', $entityType->getKey('bar'));
